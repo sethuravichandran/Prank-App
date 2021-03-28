37f42914ff387b5bd415b09fdd0ab1c8b663c4b8
Sethu Ravichandran <80445714+sethuravichandran@users.noreply.github.com>
2021-03-29 00:46:05 +0530

    script.js added

---
+++ script.js
@@ -0,0 +1,25 @@
+let counterDisplayElem = document.querySelector('.counter-display');
+let counterMinusElem = document.querySelector('.counter-minus');
+let counterPlusElem = document.querySelector('.counter-plus');
+let count = 0;
+var vid = document.getElementById("myVideo"); 
+
+updateDisplay();
+
+counterPlusElem.addEventListener("click",()=>{
+    count++;
+    updateDisplay();
+}) ;
+
+counterMinusElem.addEventListener("click",()=>{
+    count--;
+    updateDisplay();
+});
+
+function updateDisplay(){
+    counterDisplayElem.innerHTML = count;
+      if (count == 5)
+        {
+          vid.play();
+        }
+   };
